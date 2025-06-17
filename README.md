cc1fe27052f5a4551710a96eeace2dfbc9bf0cb8
Alex Parra <parraletz@gmail.com>
2025-06-16 21:54:59 -0600

    docs: Added README.md and JSDoc

--- README.md
+++ README.md
@@ -1,1 +1,194 @@
-# replace this
+# CDK VPC Pattern
+
+[![NPM version](https://img.shields.io/npm/v/@cloudscouts/cdk-vpc-pattern)](https://www.npmjs.com/package/@cloudscouts/cdk-vpc-pattern)
+[![PyPI version](https://img.shields.io/pypi/v/cdk-vpc-pattern)](https://pypi.org/project/cdk-vpc-pattern/)
+[![NuGet version](https://img.shields.io/nuget/v/CloudScoutsCdkVpcPattern)](https://www.nuget.org/packages/CloudScoutsCdkVpcPattern/)
+[![License](https://img.shields.io/github/license/parraletz/cdk-vpc-pattern)](https://github.com/parraletz/cdk-vpc-pattern/blob/main/LICENSE)
+
+A CDK construct library that provides a standardized pattern for creating AWS VPCs with public, private, and optional database subnets. This pattern includes NAT Gateways for private subnet internet access and proper tagging for Kubernetes if enabled.
+
+## Features
+
+- Creates a VPC with customizable CIDR block
+- Configures public subnets with Internet Gateway
+- Sets up private subnets with NAT Gateways
+- Optional database subnet tier
+- Kubernetes-compatible tagging for EKS clusters
+- Multi-AZ support for high availability
+- Proper naming and tagging of all resources
+
+## Installation
+
+### TypeScript/JavaScript
+
+```bash
+npm install @cloudscouts/cdk-vpc-pattern
+```
+
+### Python
+
+```bash
+pip install cdk-vpc-pattern
+```
+
+### .NET
+
+```bash
+dotnet add package CloudScoutsCdkVpcPattern
+```
+
+### Go
+
+```bash
+go get github.com/parraletz/cdk-vpc-pattern
+```
+
+## Usage
+
+### Basic VPC with Public and Private Subnets
+
+```typescript
+import { App, Stack } from "aws-cdk-lib"
+import { VpcPattern } from "@cloudscouts/cdk-vpc-pattern"
+
+const app = new App()
+const stack = new Stack(app, "MyStack")
+
+// Create a VPC with public and private subnets
+const vpc = new VpcPattern(stack, "MyVpc", {
+  name: "production-vpc",
+  cidr: "10.0.0.0/16",
+  azs: ["us-west-2a", "us-west-2b", "us-west-2c"],
+  publicSubnets: ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"],
+  privateSubnets: ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"],
+})
+
+// Access the created VPC and subnets
+const vpcId = vpc.vpcId
+const privateSubnets = vpc.privateSubnets
+const publicSubnets = vpc.publicSubnets
+```
+
+### VPC with Database Subnets
+
+```typescript
+import { App, Stack } from "aws-cdk-lib"
+import { VpcPattern } from "@cloudscouts/cdk-vpc-pattern"
+
+const app = new App()
+const stack = new Stack(app, "MyStack")
+
+// Create a VPC with public, private, and database subnets
+const vpc = new VpcPattern(stack, "MyVpc", {
+  name: "production-vpc",
+  cidr: "10.0.0.0/16",
+  azs: ["us-west-2a", "us-west-2b", "us-west-2c"],
+  publicSubnets: ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"],
+  privateSubnets: ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"],
+  databaseSubnets: ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"],
+})
+```
+
+### VPC for Kubernetes (EKS)
+
+```typescript
+import { App, Stack } from "aws-cdk-lib"
+import { VpcPattern } from "@cloudscouts/cdk-vpc-pattern"
+
+const app = new App()
+const stack = new Stack(app, "MyStack")
+
+// Create a VPC configured for EKS
+const eksVpc = new VpcPattern(stack, "EksVpc", {
+  name: "eks-vpc",
+  cidr: "10.1.0.0/16",
+  azs: ["us-west-2a", "us-west-2b"],
+  publicSubnets: ["10.1.1.0/24", "10.1.2.0/24"],
+  privateSubnets: ["10.1.3.0/24", "10.1.4.0/24"],
+  enableKubernenetes: true,
+  kubernetesClusterName: "my-eks-cluster",
+})
+```
+
+### Using the VPC with Other Constructs
+
+```typescript
+import { App, Stack } from "aws-cdk-lib"
+import * as rds from "aws-cdk-lib/aws-rds"
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2"
+import { VpcPattern } from "@cloudscouts/cdk-vpc-pattern"
+
+const app = new App()
+const stack = new Stack(app, "MyStack")
+
+const vpc = new VpcPattern(stack, "MyVpc", {
+  name: "app-vpc",
+  cidr: "10.0.0.0/16",
+  azs: ["us-west-2a", "us-west-2b"],
+  publicSubnets: ["10.0.1.0/24", "10.0.2.0/24"],
+  privateSubnets: ["10.0.3.0/24", "10.0.4.0/24"],
+  databaseSubnets: ["10.0.5.0/24", "10.0.6.0/24"],
+})
+
+// Use public subnets for a load balancer
+const alb = new elbv2.ApplicationLoadBalancer(stack, "ALB", {
+  vpc: vpc.vpcId,
+  internetFacing: true,
+  vpcSubnets: {
+    subnets: vpc.publicSubnets,
+  },
+})
+
+// Use database subnets for an RDS instance
+const dbInstance = new rds.DatabaseInstance(stack, "Database", {
+  engine: rds.DatabaseInstanceEngine.mysql({
+    version: rds.MysqlEngineVersion.VER_8_0,
+  }),
+  vpc: vpc.vpcId,
+  vpcSubnets: {
+    subnets: vpc.databaseSubnets,
+  },
+  instanceType: ec2.InstanceType.of(
+    ec2.InstanceClass.T3,
+    ec2.InstanceSize.MICRO
+  ),
+})
+```
+
+## API Reference
+
+### VpcProps
+
+Properties for creating a VPC pattern.
+
+| Property              | Type     | Required | Description                                                                 |
+| --------------------- | -------- | -------- | --------------------------------------------------------------------------- |
+| name                  | string   | Yes      | The name of the VPC                                                         |
+| cidr                  | string   | Yes      | The CIDR block for the VPC                                                  |
+| azs                   | string[] | Yes      | The availability zones to use                                               |
+| publicSubnets         | string[] | Yes      | The CIDR blocks for public subnets                                          |
+| privateSubnets        | string[] | Yes      | The CIDR blocks for private subnets                                         |
+| databaseSubnets       | string[] | No       | Optional CIDR blocks for database subnets                                   |
+| enableKubernenetes    | boolean  | No       | Whether to enable Kubernetes tags                                           |
+| kubernetesClusterName | string   | No       | The name of the Kubernetes cluster (required if enableKubernenetes is true) |
+
+### VpcPattern
+
+A construct that creates a VPC with public, private, and optional database subnets.
+
+#### Properties
+
+| Property        | Type          | Description                                            |
+| --------------- | ------------- | ------------------------------------------------------ |
+| vpcId           | ec2.IVpc      | The VPC instance                                       |
+| publicSubnets   | ec2.ISubnet[] | The public subnets created in the VPC                  |
+| privateSubnets  | ec2.ISubnet[] | The private subnets created in the VPC                 |
+| databaseSubnets | ec2.ISubnet[] | The database subnets created in the VPC (if specified) |
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit a Pull Request.
+
+## License
+
+This project is licensed under the Apache-2.0 License - see the [LICENSE](LICENSE) file for details.
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,171 @@
-import { Tags } from 'aws-cdk-lib'
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
-import { Vpc } from 'aws-cdk-lib/aws-ec2'
-import { Construct } from 'constructs'
+import { Tags } from "aws-cdk-lib"
+import * as ec2 from "aws-cdk-lib/aws-ec2"
+import { Vpc } from "aws-cdk-lib/aws-ec2"
+import { Construct } from "constructs"
 
+/**
+ * Properties for creating a VPC pattern.
+ *
+ * @example
+ * ```typescript
+ * const vpcProps: VpcProps = {
+ *   name: 'my-vpc',
+ *   cidr: '10.0.0.0/16',
+ *   azs: ['us-west-2a', 'us-west-2b', 'us-west-2c'],
+ *   publicSubnets: ['10.0.1.0/24', '10.0.2.0/24', '10.0.3.0/24'],
+ *   privateSubnets: ['10.0.4.0/24', '10.0.5.0/24', '10.0.6.0/24'],
+ *   databaseSubnets: ['10.0.7.0/24', '10.0.8.0/24', '10.0.9.0/24'],
+ *   enableKubernenetes: true,
+ *   kubernetesClusterName: 'my-eks-cluster'
+ * };
+ * ```
+ */
 export interface VpcProps {
+  /** The name of the VPC. */
   readonly name: string
+  /** The CIDR block for the VPC. */
   readonly cidr: string
+  /** The availability zones to use. */
   readonly azs: string[]
+  /** The CIDR blocks for public subnets. */
   readonly publicSubnets: string[]
+  /** The CIDR blocks for private subnets. */
   readonly privateSubnets: string[]
+  /** Optional CIDR blocks for database subnets. */
   readonly databaseSubnets?: string[]
+  /** Whether to enable Kubernetes tags. */
   readonly enableKubernenetes?: boolean
+  /** The name of the Kubernetes cluster (required if enableKubernenetes is true). */
   readonly kubernetesClusterName?: string
 }
 
+/**
+ * A construct that creates a VPC with public, private, and optional database subnets.
+ * Includes NAT Gateways for private subnet internet access and proper tagging for Kubernetes if enabled.
+ *
+ * @example
+ * ```typescript
+ * import { App, Stack } from 'aws-cdk-lib';
+ * import { VpcPattern } from './vpc-pattern';
+ *
+ * const app = new App();
+ * const stack = new Stack(app, 'MyStack');
+ *
+ * // Create a VPC with public, private, and database subnets
+ * const vpc = new VpcPattern(stack, 'MyVpc', {
+ *   name: 'production-vpc',
+ *   cidr: '10.0.0.0/16',
+ *   azs: ['us-west-2a', 'us-west-2b', 'us-west-2c'],
+ *   publicSubnets: ['10.0.1.0/24', '10.0.2.0/24', '10.0.3.0/24'],
+ *   privateSubnets: ['10.0.4.0/24', '10.0.5.0/24', '10.0.6.0/24'],
+ *   databaseSubnets: ['10.0.7.0/24', '10.0.8.0/24', '10.0.9.0/24']
+ * });
+ *
+ * // For Kubernetes use case
+ * const eksVpc = new VpcPattern(stack, 'EksVpc', {
+ *   name: 'eks-vpc',
+ *   cidr: '10.1.0.0/16',
+ *   azs: ['us-west-2a', 'us-west-2b'],
+ *   publicSubnets: ['10.1.1.0/24', '10.1.2.0/24'],
+ *   privateSubnets: ['10.1.3.0/24', '10.1.4.0/24'],
+ *   enableKubernenetes: true,
+ *   kubernetesClusterName: 'my-eks-cluster'
+ * });
+ *
+ * // Access the created VPC and subnets
+ * const vpcId = vpc.vpcId;
+ * const privateSubnets = vpc.privateSubnets;
+ * const publicSubnets = vpc.publicSubnets;
+ * ```
+ */
 export class VpcPattern extends Construct {
+  /** References to the NAT Gateways created in the VPC. */
   private natGateways: string[] = []
+
+  /**
+   * The VPC instance.
+   * @example
+   * ```typescript
+   * // Access the VPC to use in other constructs
+   * const myVpc = vpcPattern.vpcId;
+   * ```
+   */
   public vpcId: ec2.IVpc
+
+  /**
+   * The private subnets created in the VPC.
+   * @example
+   * ```typescript
+   * // Use private subnets for an RDS instance
+   * new rds.DatabaseInstance(this, 'Database', {
+   *   vpc: vpcPattern.vpcId,
+   *   vpcSubnets: {
+   *     subnets: vpcPattern.privateSubnets
+   *   },
+   *   // other properties...
+   * });
+   * ```
+   */
   public privateSubnets: ec2.ISubnet[] = []
+
+  /**
+   * The public subnets created in the VPC.
+   * @example
+   * ```typescript
+   * // Use public subnets for a load balancer
+   * new elbv2.ApplicationLoadBalancer(this, 'ALB', {
+   *   vpc: vpcPattern.vpcId,
+   *   internetFacing: true,
+   *   vpcSubnets: {
+   *     subnets: vpcPattern.publicSubnets
+   *   }
+   * });
+   * ```
+   */
   public publicSubnets: ec2.ISubnet[] = []
+
+  /**
+   * The database subnets created in the VPC (if specified).
+   * @example
+   * ```typescript
+   * // Use database subnets for an RDS instance
+   * if (vpcPattern.databaseSubnets.length > 0) {
+   *   new rds.DatabaseInstance(this, 'Database', {
+   *     vpc: vpcPattern.vpcId,
+   *     vpcSubnets: {
+   *       subnets: vpcPattern.databaseSubnets
+   *     },
+   *     // other properties...
+   *   });
+   * }
+   * ```
+   */
   public databaseSubnets: ec2.ISubnet[] = []
 
+  /**
+   * Creates a new VPC pattern.
+   * @param scope The parent construct.
+   * @param id The construct ID.
+   * @param props The VPC properties.
+   *
+   * @example
+   * ```typescript
+   * // Basic usage
+   * const vpc = new VpcPattern(this, 'MyVpc', {
+   *   name: 'app-vpc',
+   *   cidr: '10.0.0.0/16',
+   *   azs: ['us-west-2a', 'us-west-2b'],
+   *   publicSubnets: ['10.0.1.0/24', '10.0.2.0/24'],
+   *   privateSubnets: ['10.0.3.0/24', '10.0.4.0/24']
+   * });
+   * ```
+   */
   constructor(scope: Construct, id: string, props: VpcProps) {
     super(scope, id)
 
-    const azs = props?.azs || ['us-west-2a', 'us-west-2b', 'us-west-2c']
+    const azs = props?.azs || ["us-west-2a", "us-west-2b", "us-west-2c"]
 
-    const vpc = new Vpc(this, 'VPC', {
+    const vpc = new Vpc(this, "VPC", {
       vpcName: props?.name,
       maxAzs: props?.azs.length,
       ipAddresses: ec2.IpAddresses.cidr(props.cidr),
@@ -51,14 +190,14 @@ export class VpcPattern extends Construct {
         {
           vpcId: vpc.vpcId,
           availabilityZone: azs[i],
-          cidrBlock: props?.publicSubnets[i] ?? '',
+          cidrBlock: props?.publicSubnets[i] ?? "",
           mapPublicIpOnLaunch: true,
         }
       )
-      publicSubnet.addRoute('DefaultRoute', {
+      publicSubnet.addRoute("DefaultRoute", {
         routerId: internetGateway.ref,
         routerType: ec2.RouterType.GATEWAY,
-        destinationCidrBlock: '0.0.0.0/0',
+        destinationCidrBlock: "0.0.0.0/0",
       })
 
       const elasitcIP = new ec2.CfnEIP(this, `EIP-${azs[i]}`)
@@ -70,13 +209,13 @@ export class VpcPattern extends Construct {
 
       this.natGateways.push(natGateway.ref as string)
 
-      Tags.of(publicSubnet).add('Name', `${props.name}-public-${azs[i]}`)
-      Tags.of(publicSubnet).add('Tier', 'Public')
+      Tags.of(publicSubnet).add("Name", `${props.name}-public-${azs[i]}`)
+      Tags.of(publicSubnet).add("Tier", "Public")
       if (props?.enableKubernenetes) {
-        Tags.of(publicSubnet).add('kubernetes.io/role/elb', '1')
+        Tags.of(publicSubnet).add("kubernetes.io/role/elb", "1")
         Tags.of(publicSubnet).add(
           `kubernetes.io/cluster/${props.kubernetesClusterName}`,
-          'owned'
+          "owned"
         )
       }
 
@@ -90,23 +229,23 @@ export class VpcPattern extends Construct {
         {
           vpcId: vpc.vpcId,
           availabilityZone: azs[i],
-          cidrBlock: props?.privateSubnets[i] ?? '',
+          cidrBlock: props?.privateSubnets[i] ?? "",
           mapPublicIpOnLaunch: false,
         }
       )
-      privateSubnet.addRoute('DefaultRoute', {
+      privateSubnet.addRoute("DefaultRoute", {
         routerId: this.natGateways[i],
         routerType: ec2.RouterType.NAT_GATEWAY,
-        destinationCidrBlock: '0.0.0.0/0',
+        destinationCidrBlock: "0.0.0.0/0",
       })
-      Tags.of(privateSubnet).add('Name', `${props.name}-private-${azs[i]}`)
-      Tags.of(privateSubnet).add('Tier', 'Private')
+      Tags.of(privateSubnet).add("Name", `${props.name}-private-${azs[i]}`)
+      Tags.of(privateSubnet).add("Tier", "Private")
 
       if (props?.enableKubernenetes) {
-        Tags.of(privateSubnet).add('kubernetes.io/role/internal-elb', '1')
+        Tags.of(privateSubnet).add("kubernetes.io/role/internal-elb", "1")
         Tags.of(privateSubnet).add(
           `kubernetes.io/cluster/${props.kubernetesClusterName}`,
-          'owned'
+          "owned"
         )
       }
       this.privateSubnets.push(privateSubnet)
@@ -120,12 +259,12 @@ export class VpcPattern extends Construct {
           {
             vpcId: vpc.vpcId,
             availabilityZone: azs[i],
-            cidrBlock: props?.databaseSubnets[i] ?? '',
+            cidrBlock: props?.databaseSubnets[i] ?? "",
             mapPublicIpOnLaunch: false,
           }
         )
-        Tags.of(databaseSubnet).add('Name', `${props.name}-database-${azs[i]}`)
-        Tags.of(databaseSubnet).add('Tier', 'Private')
+        Tags.of(databaseSubnet).add("Name", `${props.name}-database-${azs[i]}`)
+        Tags.of(databaseSubnet).add("Tier", "Private")
       }
     }
     this.vpcId = vpc
